e58e0beb06eb3b26cc0bb804c3fd74c0ee3e9143
codygplunkett <cody.plunkett@yahoo.com>
2018-09-19 15:10:06 -0500

    files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# MyFishingJournal
-Final Project
+## My Fishing Journal
+
+### Purpose
+
+The purpose of My Fishing Journal is to create a place for fisherman to post journal entries about fishing trips they take. Proffesional guides all keep journals of their own fishing data, and amateurs should as well.
+It will also allow fisherman to see other peoples public journal entries as well to scout a fishing area.
+
+### Features
+
+Allows fisherman to place google maps geo locations, keep track of weather conditions, what gear and bait they used as well as techniques, and what kind of success they had. It will also allow you to see what other fisherman in the area have had succes doing as well.
+
+### Instrucions
+
+Unfortunately the app is not currently deployable, but is still being worked on and will be in the near future as of sep,2018.
+
+### Authors
+Robert Lipson, Cody Plunkett
---
+++ maps/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+           
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyAn9AgRPfrvF1DaYJxtoppcjgFDtMh0Pz8")
+})(MapContainer)
+
